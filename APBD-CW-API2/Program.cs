aa3819cebd120130fd20d0f842cb01ff412e0500
Olaf Mickiewicz <olaf@mickiewicz.pro>
2025-05-02 13:37:45 +0200

    Program.cs sie kompiluje

--- APBD-CW-API2/Program.cs
+++ APBD-CW-API2/Program.cs
@@ -1,5 +1,5 @@
 
-//using APBD_CW_API2.Services;
+using APBD_CW_API2.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-//builder.AddTransient<ISqlService, SqlService>();
+builder.Services.AddTransient<IDbService, DbService>();
 
 var app = builder.Build();
 
---
+++ APBD-CW-API2/Services/DbService.cs
@@ -0,0 +1,14 @@
+﻿namespace APBD_CW_API2.Services;
+
+public interface IDbService
+{
+    
+    
+    
+    
+}
+
+public class DbService(IConfiguration config) : IDbService
+{
+    
+}
